33c765182e321044e06a664e51262a93a65919ce
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-08-17 00:38:44 +0800

    Added IAsyncInitializable.cs

---
+++ FlorenceSharp/IAsyncInitializable.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace FlorenceSharp
+{
+    public interface IAsyncInitializable<ThisT> where ThisT: IAsyncInitializable<ThisT>
+    {
+        public static abstract ValueTask<ThisT> InitializeAsync();
+    }
+}
